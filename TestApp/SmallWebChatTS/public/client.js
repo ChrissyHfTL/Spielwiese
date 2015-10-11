2f2b6c287e7322224933cc67d955ce9d14021d6d
Tobias Klauenberg <s143512@hft-leipzig.de>
2015-10-11 19:47:31 +0200

    Web Chat erst einmal auf TypeScript und Jade umgestellt.

---
+++ TestApp/SmallWebChatTS/public/client.js
@@ -0,0 +1,133 @@
+﻿$(function () {
+    var $window = $(window);
+    var $usernameInput = $('.usernameInput'); // Inputfeld für den Username
+    var $messages = $('.messages'); // Chatfenster
+    var $inputMessage = $('.inputMessage'); // Inputfeld für die Nachricht
+    
+    var $loginPage = $('.login.page'); // Loginseite
+    var $chatPage = $('.chat.page'); // Chatseite
+    
+    var username;
+    var connected = false;
+    var $currentInput = $usernameInput.focus();
+    
+    var socket = io();
+    
+    // Fügt eine neue Chatzeile als Listeneintrag zum Chat hinzu und scrollt zum Ende.
+    function addMessageElement(message, options) {
+        var $el = $('<li>').addClass('log').text(message);
+        $el = $($el);
+        
+        // Optionen auf das Listenobjekt anwenden.
+
+        // Den Listeneintrag langsam einblenden lassen.
+        if (options.fade) {
+            $el.hide().fadeIn(150);
+        }
+        // Den Listeneintrag vorneanstellen.
+        if (options.prepend) {
+            $messages.prepend($el);
+        } 
+        // Ansonsten hinten anfügen.
+        else {
+            $messages.append($el);
+        }
+        // Die gesamte Liste (also der Chat) nach unten scrollen.
+        $messages[0].scrollTop = $messages[0].scrollHeight;
+    }
+    
+    // Fügt eine Nachricht zum Chat hinzu.
+    function addChatMessage() {
+        var message = $inputMessage.val();
+        // Prüfung, ob die Verbindung vorhanden ist und die Nachricht nicht leer ist
+        if (message && connected) {
+            // Inputfeld wieder leeren
+            $inputMessage.val('');
+            // Die Nachricht über die addMessageElement Funktion einfügen.
+            addMessageElement(username + ': ' + message, { prepend: false, fade: true });
+            // Den Server über die neue Nachricht unterrichten
+            socket.emit('new message', message);
+        }
+    }
+
+    // Fügt die aktuelle Anzahl der eingeloggte Nutzer zum Chat hinzu.
+    function addUserAmount(data){
+        // Entscheidung des Nachrichtentextes je nach Anzahl der Nutzer (Mehrzahl und so).
+        if (data.numUsers === 1) {
+            addMessageElement('Es ist aktuell 1 Nutzer online.', { prepend: false, fade: false });
+        }
+        else {
+            addMessageElement('Es sind aktuell ' + data.numUsers + ' Nutzer online.', { prepend: false, fade: false });
+        }
+    }
+    
+    // Tastatur Events
+    $window.keydown(function (event) {
+        // Wenn der Nutzer ENTER drückt
+        if (event.which === 13) {
+            // Prüfen, ob der Nutzer bereits einen Usernamen eingegeben hat.
+            if (username) {
+                addChatMessage();
+            }
+            else {
+                username = $usernameInput.val().trim();
+                // Wenn der Username valide ist.
+                if (username) {
+                    // Login Seite ausblenden und Chat Seite anzeigen
+                    $loginPage.fadeOut();
+                    $chatPage.show();
+                    $loginPage.off('click');
+                    // Fokus auf das neue Inputfeld
+                    $currentInput = $inputMessage.focus();
+                    // Dem Server den eigenen Usernamen bekannt machen.
+                    socket.emit('add user', username);
+                }
+            }
+        }
+    });
+
+    // Klick Events
+    // Focus input when clicking anywhere on login page
+    $loginPage.click(function () {
+        $currentInput.focus();
+    });
+    
+    // Focus input when clicking on the message input's border
+    $inputMessage.click(function () {
+        $inputMessage.focus();
+    });
+    
+    // Horcht darauf, ob der Server "login" verschickt.
+    socket.on('login', function (data) {
+        // Die Client Variablen ändern
+        connected = true;
+        // Die Willkommensnachricht im Chatfenster anzeigen.
+        addMessageElement('Willkommen beim Specturtly Web Chat', { prepend: false, fade: false });
+        // Die aktuelle Anzahl der Nutzer wird angezeigt.
+        addUserAmount(data);
+    });
+    
+    // Horcht darauf, ob der Server "new message" verschickt.
+    socket.on('new message', function (data) {
+        // Die Benachrichtigung über den neuen Nutzer im Chatfenster anzeigen.
+        addMessageElement(data.message, { prepend: false, fade: true });
+        //addChatMessage(data, { prepend: true, fade: false });
+    });
+    
+    // Horcht darauf, ob der Server "user joined" verschickt.
+    socket.on('user joined', function (data) {
+        // Die Benachrichtigung über den neuen Nutzer im Chatfenster anzeigen.
+        addMessageElement(data.username + ' ist jetzt online.', { prepend: false, fade: false });
+        // Die aktuelle Anzahl der Nutzer wird angezeigt.
+        addUserAmount(data);
+    });
+    
+    // Horcht darauf, ob der Server "user left" verschickt.
+    socket.on('user left', function (data) {
+        // Die Benachrichtigung über den verlassenen Nutzer im Chatfenster anzeigen.
+        addMessageElement(data.username + ' ist jetzt offline.', { prepend: false, fade: false });
+        // Die aktuelle Anzahl der Nutzer wird angezeigt.
+        addUserAmount(data);
+    });
+
+});
---
+++ TestApp/SmallWebChatTS/server.ts
@@ -0,0 +1,79 @@
+﻿// Einen Standard Express Server starten.
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var config = require('./config.json');
+
+// Den Server auf den Port aus der config.json horchen lassen.
+server.listen(config.port, function () {
+    console.log('Server listening at port %d', config.port);
+});
+
+// Das Routing zum public Ordner einstellen, damit die Clients dort (und nur dort) Zugriff bekommen.
+app.use(express.static(__dirname + '/public'));
+
+//benutzen der jade index Datei
+app.set('view engine', 'jade');
+app.set('views', './public');
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// ################## CHAT ROOM ##################
+
+// Variablen um die eingeloggten Nutzer und die Anzahl nachzuverfolgen.
+var usernames = {};
+var numUsers = 0;
+
+// Hauptfunktion: Hier wird eine komplette Verbindung behandelt. Diese wird eine Verbindung aufgebaut, sobald ein Nutzer auf die Seite navigiert.
+io.on('connection', function (socket) {
+    // Initiale Belegung, dass der User noch nicht "registriert" ist.
+    var addedUser = false;
+    
+    // Horcht darauf, ob ein Nutzer "new message" verschickt.
+    socket.on('new message', function (data) {
+        // Allen Clients wird mitgeteilt, dass eine neue Nachricht zur Verfügung steht.
+        // Dazu wird ein Broadcast gemacht und die Informationen des Nutzers, der die Nachricht geschrieben hat, mitverschickt.
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+    
+    // Horcht darauf, ob ein Nutzer "add user" verschickt.
+    socket.on('add user', function (username) {
+        // Der Username des Nutzers wird in der Socket Session für den Client gespeichert.
+        socket.username = username;
+        // Der Client wird der globalen Variable aller Nutzer hinzugefügt.
+        usernames[username] = username;
+        ++numUsers;
+        // Der User ist nun vollständig eingeloggt.
+        addedUser = true;
+        // Dem Client wird der eingeloggte Zustand über "login" mitgeteilt und die Anzahl der Nutzer übergeben.
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // Es wird eine globale Nachricht über "user joined" versendet mit den Informationen des neuen Nutzers.
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+    
+    // Horcht darauf, ob ein Nutzer "disconnect" verschickt.
+    socket.on('disconnect', function () {
+        // Der Client wird von der globalen Variable aller Nutzer entfernt.
+        // Es wird dazu zuvor geprüft, ob der Nutzer überhaupt korrekt eingeloggt war.
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+            
+            // Es wird eine globale Nachricht über "user left" versendet mit den Informationen des ausgeloggten Nutzers.
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
