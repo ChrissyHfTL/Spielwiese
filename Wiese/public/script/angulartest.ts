93a30b2af2817e6cd53f9818c2a1c5bc5bbfd432
Tobias Klauenberg <s143512@hft-leipzig.de>
2015-10-18 19:59:33 +0200

    Definitionsdateien für socket.io hinzugefügt (serer und  Client).

---
+++ Wiese/public/script/angulartest.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../node_modules/angular2/ts/angular2" />
+/// <reference path="../../typings/socket.io/socket.io.d.ts" />
+
+import {bootstrap, Component, View, NgFor} from "angular2/angular2";
